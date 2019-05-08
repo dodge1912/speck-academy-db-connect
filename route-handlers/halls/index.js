2e69d1fdea9845213d3d04ff56886b3a1723305e
dodge1912 <ivang1912@gmail.com>
2019-05-08 17:45:16 +0200

    add router for list, get, create, update and delete halls

--- route-handlers/halls/index.js
+++ route-handlers/halls/index.js
@@ -3,5 +3,10 @@ const hallsController = require('./controller');
 const router = new express.Router();
 
 router.route('/').get(hallsController.getHalls);
+router.route('/:hall_uid').get(hallsController.getHallById);
+router.route('/create').post(hallsController.createHall);
+router.route('/update/:hall_uid').put(hallsController.updateHall);
+router.route('/delete/:hall_uid').delete(hallsController.deleteHall);
+
 
 module.exports = router;
